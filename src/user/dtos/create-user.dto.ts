100f9ae6cedcd4cf6ba8fe6ae5d76f5da396f30f
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-12-08 01:12:50 +0530

    setup signup route

---
+++ src/user/dtos/create-user.dto.ts
@@ -0,0 +1,8 @@
+import { IsEmail, IsString } from 'class-validator';
+import { User } from '../user.entity';
+
+export class CreateUserDto extends User {
+	@IsEmail() email: string;
+	@IsString() username: string;
+	@IsString() password: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(@InjectRepository(User) private readonly userRepo: Repository<User>) {}
+
+	async findAll(): Promise<User[]> {
+		return await this.userRepo.find();
+	}
+
+	async create(email: string, password: string): Promise<User> {
+		const newUser = this.userRepo.create({ email, password });
+		return await this.userRepo.save(newUser);
+	}
+}
