276f2b9891e0b09fff17b3b17d41334133eacb30
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-12-09 01:30:04 +0530

    added custom serializer

---
+++ src/interceptors/SerializeInterceptor.ts
@@ -0,0 +1,26 @@
+import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+interface ClassConstructor {
+	new (...args: any[]): any;
+}
+
+export const Serialize = (dto: ClassConstructor) => UseInterceptors(new SerializeInterceptor(dto));
+
+export class SerializeInterceptor implements NestInterceptor {
+	constructor(private dto: ClassConstructor) {}
+
+	intercept(
+		context: ExecutionContext,
+		next: CallHandler<any>,
+	): Observable<any> | Promise<Observable<any>> {
+		return next.handle().pipe(
+			map((data: any) => {
+				return plainToClass(this.dto, data, {
+					excludeExtraneousValues: true,
+				});
+			}),
+		);
+	}
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/SerializeInterceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
+@Serialize(UserDto)
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
